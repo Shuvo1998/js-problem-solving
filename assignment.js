b723ece64df242b4b06156bc2c33c0a692f33dfa
shuvo1998 <sk.ray1998t@gmail.com>
2021-01-22 17:35:27 +0600

    budgetCalculator added

--- assignment.js
+++ assignment.js
@@ -8,4 +8,18 @@ function kilometerToMeter(kilometer){
         return "Distance cannot be negative!";
     }
 }
-console.log(kilometerToMeter(1));
+// console.log(kilometerToMeter(1));
+// console.log(kilometerToMeter(-1));
+//a function to calculate budget of buying electronics
+function budgetCalculator(quantityOfWatch, quantityOfPhone, quantityOfLaptop){
+    if(quantityOfWatch > 0 || quantityOfPhone > 0 || quantityOfLaptop >0){
+        var costOfWatch = quantityOfWatch * 50;
+        var costOfPhone = quantityOfPhone * 100;
+        var costOfLaptop = quantityOfLaptop * 500;
+        var totalCost = costOfWatch + costOfPhone + costOfLaptop;
+        return totalCost;
+    }else{
+        return "Quantity of electronics cannot be negative or others!";
+    }
+}
+console.log(budgetCalculator(1,2,3));
